71761aca12862f7b38b4586113cede3e632cded5
Janani <92579981+janani-gopi@users.noreply.github.com>
2023-05-18 11:23:39 +0530

    Add files via upload

---
+++ expense-tracker/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import React from 'react'
+import { Expense_tracker } from './Expense_tracker/Expense_tracker'
+
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Expense_tracker/>  
+    </div>
+      )
+
+  }      
