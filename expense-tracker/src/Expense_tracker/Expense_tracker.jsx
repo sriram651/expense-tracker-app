bc38b0668e035e64071e943d6ca3352558668c5d
Sriram Venkatesh <vsriram353@gmail.com>
2023-06-08 17:11:58 +0530

    Transactions Display and Date

--- expense-tracker/src/Expense_tracker/Expense_tracker.jsx
+++ expense-tracker/src/Expense_tracker/Expense_tracker.jsx
@@ -2,87 +2,105 @@ import React from 'react'
 import { useState } from 'react'
 
 export const Expense_tracker = () => {
-  const [resultsArray,setResultsArray]=useState([])
-  const [enteredAmount,setEnteredAmount]=useState(0) 
-  const [totalAmount,setTotalAmount]=useState(0)
-  let action = ""
+  const [resultsArray, setResultsArray] = useState([])
+  const [enteredAmount, setEnteredAmount] = useState(0)
+  const [totalAmount, setTotalAmount] = useState(0)
+  let action = "";
+
+  React.useEffect(() => {
+    console.log(resultsArray);
+  }, [resultsArray]);
   return (
     <div>
-        <div className='header-div'>
-              <h1>Expense tracker</h1>
-              <div className="header">
-                 <h3>Balance :<span className='balance'>{totalAmount<0 ? setTotalAmount(0) : totalAmount
-                        }</span></h3>
-                 <input type="number" value={enteredAmount} onChange={amountChange} className='input' />
-                <div>
-                   <button onClick={increase}>Add</button>
-                   <button onClick={decrease}>Remove</button>
-               </div>
-             
+      <div className='header-div'>
+        <h1>Expense tracker</h1>
+        <div className="header">
+          <h3>Balance :<span className='balance'>{totalAmount < 0 ? setTotalAmount(0) : totalAmount
+          }</span></h3>
+          <input 
+            required
+            type="number" 
+            value={enteredAmount} 
+            onChange={amountChange} 
+            className='input' />
+          <div>
+            <button onClick={increase}>Add</button>
+            <button onClick={decrease}>Remove</button>
+          </div>
+
+        </div>
+      </div>
+      <div className="transaction-div">
+        <h2>Transactions : </h2>
+        <div className="container">
+          {resultsArray && resultsArray.map((data) => {
+            return (
+              <div key={data}>
+                <p>Rs. {data.transactionAmount}/-</p>
+                {data.transactionType == "Add" && <p style={{color: "green"}}>Deposited</p>}
+                {data.transactionType == "Remove" && <p style={{color: "Red"}}>Withdrawn</p>}
+                <h5>On {data.transactionDate}  {data.transactionTime}</h5>
               </div>
-         </div>
-        <div className="transaction-div">
-          <h2>Transactions : </h2>
-           <div className="container">
-              { resultsArray.map((data)=>{
-                return(<h3 key={data}>{data}</h3>)
-              })
-              }
-           </div>
-        </div> 
+            )
+          })
+          }
+          {resultsArray.length == 0 && <h3>No Transactions to display!</h3>}
+        </div>
+      </div>
     </div>
-    
-    
+
+
   )
   function getResultswithDate() {
-    const daysArray = ["S","M" ,"T","W","T", "F","S"]
-    const date = new Date().getDate() //current date
-    const month = new Date().getMonth() + 1 //current month
-    const year = new Date().getFullYear() //current year
-    const hours = new Date().getHours() //current hours
-    const min = new Date().getMinutes() //current minutes
-    const sec = new Date().getSeconds() //current seconds
-    const dayIndex = new Date().getDay() //current seconds
-    
-    return year+ '-' + month + '-' + date + ' ' + daysArray[dayIndex] + hours + ':' + min + ':' + sec + '-' + enteredAmount + '-'+ action
+    const date = new Date() //current date
+    const dateString = date.toLocaleDateString(undefined, {year: 'numeric', month: 'numeric', day: 'numeric'});
+    const timeString = date.toLocaleTimeString();
+  
+    return {
+      transactionAmount: enteredAmount,
+      transactionDate: dateString,
+      transactionTime: timeString,
+      transactionType: action
+    }
   }
-   
 
-   function amountChange(e){
-     setEnteredAmount(e.target.value)
-    
-   }
 
-   function noAmountEntered(){
+  function amountChange(e) {
+    setEnteredAmount(e.target.value)
+  }
+
+  function noAmountEntered() {
     alert("Please enter some amount first")
-   }
+  }
 
-   function decrease(e){
-   if(enteredAmount){
-       if(totalAmount === 0){
-         alert("you have no balance!!")
-        }
+  function decrease(e) {
+    if (enteredAmount) {
+      if (totalAmount === 0) {
+        alert("you have no balance!!")
+      }
 
-        else if(enteredAmount > totalAmount){
-         alert("Entered amount is more than your balance")
-        }
-       else{
+      else if (enteredAmount > totalAmount) {
+        alert("Entered amount is more than your balance")
+      }
+      else {
         action = (e.target.innerText)
         setTotalAmount(totalAmount - enteredAmount)
         setEnteredAmount('')
-        setResultsArray(oldArray => [...oldArray,getResultswithDate()])
-        }}else{
-          noAmountEntered()
-        }
+        setResultsArray(oldArray => [...oldArray, getResultswithDate()])
+      }
+    } else {
+      noAmountEntered()
+    }
   }
-  function increase(e){
-    if(enteredAmount){
-    setTotalAmount(totalAmount +  parseInt(enteredAmount))
-    setEnteredAmount('')
-    setResultsArray(oldArray => [...oldArray,getResultswithDate()])
-   action =  (e.target.innerText)}
-   else {
-    noAmountEntered()
-   }    
+  function increase(e) {
+    if (enteredAmount) {
+      setTotalAmount(totalAmount + parseInt(enteredAmount))
+      setEnteredAmount('')
+      setResultsArray(oldArray => [...oldArray, getResultswithDate()])
+      action = (e.target.innerText)
+    }
+    else {
+      noAmountEntered()
+    }
   }
 }
